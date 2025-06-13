aab0f306b0c62c8cf40c5121c5666993cc2e6a2a
Walden-Graphic <68792745+Walden-Graphic@users.noreply.github.com>
2025-06-13 13:58:11 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ color: 'lime', backgroundColor: 'black', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <h1>J6 Online</h1>
+    </div>
+  );
+}
+
+export default App;
