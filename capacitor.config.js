b84f53cf20bc8c30c1a26c2f58c19b064fe4599c
Walden-Graphic <68792745+Walden-Graphic@users.noreply.github.com>
2025-06-13 14:00:09 -0700

    Create capacitor.config.js

---
+++ capacitor.config.js
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.j6.app',
+  appName: 'J6',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
