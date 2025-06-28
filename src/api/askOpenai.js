bfebc16a1fedd58bd70714d79bf65b40b1dabf2c
Walden-Graphic <68792745+Walden-Graphic@users.noreply.github.com>
2025-06-28 07:23:35 -0700

    Update askOpenai.js

--- src/api/askOpenai.js
+++ src/api/askOpenai.js
@@ -1,7 +1,7 @@
 import { OpenAI } from 'openai';
 
 const openai = new OpenAI({
-  apiKey: 'sk-proj-xxx', // use your actual key
+  apiKey: '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',
   dangerouslyAllowBrowser: true,
 });
 
