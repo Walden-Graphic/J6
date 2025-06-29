7e013f86eb942a6fef5749144fc82c0a489779e5
Walden-Graphic <68792745+Walden-Graphic@users.noreply.github.com>
2025-06-29 07:08:05 -0700

    Update MainActivity.java

--- android/app/src/main/java/com/j6/MainActivity.java
+++ android/app/src/main/java/com/j6/MainActivity.java
@@ -1,1 +1,7 @@
-// MainActivity.java placeholder content
+package com.j6;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    // You can leave this empty unless you're adding custom native plugins
+}
